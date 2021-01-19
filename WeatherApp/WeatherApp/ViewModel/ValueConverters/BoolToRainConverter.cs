0da2f33a789cd697802e338480eee56990d8708d
AnchitPandey <anchitpa@usc.edu>
2021-01-19 14:30:58 -0800

    Uploading WPF App

---
+++ WeatherApp/WeatherApp/ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.ViewModel.ValueConverters
+{
+    class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+            if (isRaining)
+                return "Currently raining";            
+            return "Currently not raining";
+           // throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string isRaining = value as string;
+            if (isRaining == "Currently raining")
+                return true;
+            return false;
+          //  throw new NotImplementedException();
+        }
+    }
+}
